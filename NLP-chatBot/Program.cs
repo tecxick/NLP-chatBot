37ae8e6c1be72bca4cb42a4d847156723474c69d
Pasan Anuththara <pasananuththara19@gmail.com>
2021-03-01 18:03:33 +0530

    Trainer

--- NLP-chatBot/Program.cs
+++ NLP-chatBot/Program.cs
@@ -21,59 +21,71 @@ namespace NLP_chatBot
 
         static void Main(string[] args)
         {
-            //the input text
-            var text = new Sentence("This is a test line. and this should work well.");
+            while (true) {
 
-            // Lemmas
-            var lemmas = text.Lemmas;
+                Console.WriteLine("Enter input ");
 
-            //POS Tags
-            var pos = text.PosTags;
+                //the input text
+                var text = new Sentence(Console.ReadLine());
 
-            // Weather the sentence is positive or negative
-            var sen = text.Sentiment;
+                // Lemmas
+                var lemmas = text.Lemmas;
 
-            // Record the word with its properties that we can use later to make decesions
-            var words = new List<word>();
+                //POS Tags
+                var pos = text.PosTags;
 
-            // Printout the original words indevidually
-            foreach (var r in text.OriginalWords)
-            {
-                Console.WriteLine(r + " :: ");
+                // Weather the sentence is positive or negative
+                var sen = text.Sentiment;
+
+                // Record the word with its properties that we can use later to make decesions
+                var words = new List<word>();
+
+                // Printout the original words indevidually
+                foreach (var r in text.OriginalWords)
+                {
+                    Console.WriteLine(r + " :: ");
+                }
+
+                Console.WriteLine("###########");
+
+                // print out and record the words with their props
+                for (int noW = 0; noW < text.OriginalWords.Count; noW++)
+                {
+                    Console.WriteLine(lemmas.ToArray()[noW] + ">" + pos.ToArray()[noW]);
+                    // wordd is the struct i madeabove
+                    words.Add(new word { Original = text.OriginalWords.ToArray()[noW], Lemma = lemmas.ToArray()[noW], PosTag = pos.ToArray()[noW] });
+                }
+
+                Console.WriteLine("+++++++++++++");
+
+                //lets test it out;
+                foreach (var w in words)
+                {
+                    // print all saved words
+                    Console.WriteLine(w.Original + ":::" + w.Lemma + ":::" + w.PosTag);
+                    Console.Beep(2200, 100);
+                }
+
+                Console.WriteLine(sen);
+
+                Console.WriteLine(text.OriginalWords);
+
+
+                #region Cnf
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+
+                Console.ReadKey();
+                Console.Beep();
+                #endregion
+
+                Console.WriteLine("Re-run?  (y/n)");
+                if (Console.ReadLine() != "y")
+                {
+                    break;
+                }
             }
-
-            Console.WriteLine("###########");
-
-            // print out and record the words with their props
-            for (int noW = 0; noW<text.OriginalWords.Count; noW++)
-            {
-                Console.WriteLine(lemmas.ToArray()[noW] + ">" + pos.ToArray()[noW]);
-                // wordd is the struct i madeabove
-                words.Add(new word { Original = text.OriginalWords.ToArray()[noW], Lemma = lemmas.ToArray()[noW], PosTag = pos.ToArray()[noW] });
-            }
-
-            Console.WriteLine("+++++++++++++");
-
-            //lets test it out;
-            foreach (var w in words)
-            {
-                // print all saved words
-                Console.WriteLine(w.Original + ":::" + w.Lemma + ":::" + w.PosTag);
-            }
-
-            Console.WriteLine(sen);
-
-            Console.WriteLine(text.OriginalWords);
-
-
-            #region Cnf
-            Console.Beep();
-            Console.Beep();
-            Console.Beep();
-
-            Console.ReadKey();
-            Console.Beep();
-            #endregion
         }
     }
 }
